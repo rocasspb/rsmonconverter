07be5510f6af4b57df577af2a39eea53ae9a9996
Sergey Afanasiev <rocas.spb@gmail.com>
2018-08-20 12:07:59 +0200

    cleanup

--- src/RaceRenderCSVDataWriter.kt
+++ src/RaceRenderCSVDataWriter.kt
@@ -1,4 +1,3 @@
-import com.sun.org.apache.xpath.internal.operations.Bool
 import java.io.PrintWriter
 
 class RaceRenderCSVDataWriter(private val output: PrintWriter) : OutputDataWriter<DataLine>  {
